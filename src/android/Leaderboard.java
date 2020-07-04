5e0aead37679cd1e1fb8db9b6cfb49f0d34423d0
PrinceSanjivy <sanjivy.android@gmail.com>
2020-07-04 16:52:51 +0530

    added Leaderboard.java

---
+++ src/android/Leaderboard.java
@@ -0,0 +1,74 @@
+package com.princewebstudio.plugin;
+
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.apache.cordova.CallbackContext;
+
+
+import android.content.Intent;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.games.Games;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class Leaderboard extends CordovaPlugin{
+  private static final int RC_SIGN_IN = 9001;
+  private static final int RC_LEADERBOARD_UI = 9004;
+
+  public CordovaPlugin reference = this;
+  public CallbackContext callback;
+
+  @Override
+  public void initialize(CordovaInterface cordova, CordovaWebView webview){
+    super.initialize(cordova, webview);
+  }
+
+  @Override
+  public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException{
+    if(action.equals("init")){
+      callback = callbackContext;
+
+      GoogleSignInOptions signInOptions = GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN;
+      GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(this.cordova.getActivity(), signInOptions);
+      Intent intent = googleSignInClient.getSignInIntent();
+
+      cordova.setActivityResultCallback(this);
+      cordova.startActivityForResult(this, intent, RC_SIGN_IN);
+
+      return true;
+    }
+    else if(action.equals("setScore")){
+      callback = callbackContext;
+
+      String leaderboard_id = data.getString(0);
+      int score = Integer.valueOf(data.getString(1));
+
+      setScore(leaderboard_id, score);
+
+      return true;
+    }
+    else if(action.equals("showLeaderboard")){
+      callback = callbackContext;
+
+      String leaderboard_id = data.getString(0);
+
+      showLeaderboard(leaderboard_id);
+
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
